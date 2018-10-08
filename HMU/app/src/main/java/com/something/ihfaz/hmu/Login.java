469d997399f9c0ceac25c1231da8fe14bfda26a0
Ihfaz <ihfaz.98@gmail.com>
2018-10-07 19:41:30 -0500

    Email link

--- HMU/app/src/main/java/com/something/ihfaz/hmu/Login.java
+++ HMU/app/src/main/java/com/something/ihfaz/hmu/Login.java
@@ -2,12 +2,27 @@ package com.something.ihfaz.hmu;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class Login extends AppCompatActivity {
+
+    EditText email;
+    EditText pass;
+    Button login;
+    TextView reg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        email = (EditText) findViewById(R.id.etEmail);
+        pass = (EditText) findViewById(R.id.etPass);
+        login = (Button) findViewById(R.id.bLogin);
+        reg = (TextView) findViewById(R.id.tvRegister);
+
+        
     }
 }
--- HMU/app/src/main/java/com/something/ihfaz/hmu/Registration.java
+++ HMU/app/src/main/java/com/something/ihfaz/hmu/Registration.java
@@ -2,12 +2,29 @@ package com.something.ihfaz.hmu;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class Registration extends AppCompatActivity {
+
+    EditText email;
+    EditText name;
+    EditText pass;
+    EditText pass2;
+    Button enter;
+    TextView user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+
+        email = (EditText) findViewById(R.id.etrEmail);
+        name = (EditText) findViewById(R.id.etrName);
+        pass = (EditText) findViewById(R.id.etrPass);
+        pass2 = (EditText) findViewById(R.id.etrPass2);
+        enter = (Button) findViewById(R.id.bEnter);
+        user = (TextView) findViewById(R.id.tvUser);
     }
 }
