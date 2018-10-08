ef32f477b91de99bf996bb91e73046da95233dfb
Ihfaz <ihfaz.98@gmail.com>
2018-10-07 20:43:26 -0500

    Email link

--- HMU/app/src/main/java/com/something/ihfaz/hmu/Login.java
+++ HMU/app/src/main/java/com/something/ihfaz/hmu/Login.java
@@ -1,7 +1,16 @@
 package com.something.ihfaz.hmu;
 
+import android.content.Intent;
+import android.graphics.Color;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.SpannableStringBuilder;
+import android.text.Spanned;
+import android.text.method.LinkMovementMethod;
+import android.text.style.ClickableSpan;
+import android.text.style.ForegroundColorSpan;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -23,6 +32,37 @@ public class Login extends AppCompatActivity {
         login = (Button) findViewById(R.id.bLogin);
         reg = (TextView) findViewById(R.id.tvRegister);
 
-        
+        String text = "Not registered yet? Register here\n Forgot your password?";
+        SpannableStringBuilder ssb = new SpannableStringBuilder(text);
+
+        ClickableSpan cs1 = new ClickableSpan() {
+            @Override
+            public void onClick(@NonNull View widget) {
+                toNewReg();
+            }
+        };
+
+        ClickableSpan cs2 = new ClickableSpan() {
+            @Override
+            public void onClick(@NonNull View widget) {
+                toForgotPass();
+            }
+        };
+
+        ssb.setSpan(cs1, 20, 33, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        ssb.setSpan(cs2, 34, 56, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+        reg.setText(ssb);
+        reg.setMovementMethod(LinkMovementMethod.getInstance());
+    }
+
+    public void toNewReg(){
+        Intent intentNR = new Intent(this, Email.class);
+        startActivity(intentNR);
+    }
+
+    public void toForgotPass(){
+        Intent intentFP = new Intent(this, Email.class);
+        startActivity(intentFP);
     }
 }
