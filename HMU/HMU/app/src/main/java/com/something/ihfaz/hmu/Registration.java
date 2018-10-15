84d4b9981d685424dd89bbb71802927c7af95991
Noah <noahcastetter@gmail.com>
2018-10-15 10:54:27 -0500

    Integrated Firestore and Dependencies. Wrote Partial Back-end. Connected to Back End

--- HMU/HMU/app/src/main/java/com/something/ihfaz/hmu/Registration.java
+++ HMU/HMU/app/src/main/java/com/something/ihfaz/hmu/Registration.java
@@ -1,6 +1,7 @@
 package com.something.ihfaz.hmu;
 
 import android.content.Intent;
+import android.nfc.Tag;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -9,10 +10,18 @@ import android.text.Spanned;
 import android.text.TextPaint;
 import android.text.method.LinkMovementMethod;
 import android.text.style.ClickableSpan;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class Registration extends AppCompatActivity {
 
@@ -23,20 +32,50 @@ public class Registration extends AppCompatActivity {
     Button enter;
     TextView user;
 
+    FirebaseFirestore db = FirebaseFirestore.getInstance();  //creating instance of firestore
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
 
-        email = (EditText) findViewById(R.id.etrEmail);
+        email = (EditText) findViewById(R.id.etrEmail); //adding listeners from 31 to line 36
         name = (EditText) findViewById(R.id.etrName);
         pass = (EditText) findViewById(R.id.etrPass);
         pass2 = (EditText) findViewById(R.id.etrPass2);
-        enter = (Button) findViewById(R.id.bEnter);
-        user = (TextView) findViewById(R.id.tvUser);
+        enter = (Button) findViewById(R.id.bEnter);     //!button to submit form!
+        user = (TextView) findViewById(R.id.tvUser);    //this is making the button for people who are already registered
 
-        String text = "Already a user? Click here";
-        SpannableStringBuilder ssb = new SpannableStringBuilder(text);
+        String text = "Already a user? Click here";                         //same here
+        SpannableStringBuilder ssb = new SpannableStringBuilder(text);      //displaying the text
+
+        //submitting contents of email, name, pass, and pass2 when "enter" button is pressed
+
+        enter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Map<String, Object> user = new HashMap<>();
+                //TODO put netid here
+                user.put("email", email.getText().toString());
+                user.put("display-name", name.getText().toString());
+                //TODO put password verify here
+                user.put("password", pass.getText().toString());
+
+
+                // adding the new user doc
+
+                 db.collection("users").document("newDocumentId")
+                         .set(user);
+            }
+        });
+
+
+
+
+
+
+
+        //Text at bottom that goes back to login screen
 
         ClickableSpan cs1 = new ClickableSpan() {
             @Override
